b7be80dd9c060db08921469dbce49f11acbd6fd9
ArsalanTahir1 <arslanlive1@gmail.com>
2021-05-03 16:18:57 +0500

    Week 4 work. We have implemented how to navigate to new activity and a menu for settings

--- app/src/main/java/com/example/firstapp/MainActivity.kt
+++ app/src/main/java/com/example/firstapp/MainActivity.kt
@@ -1,25 +1,50 @@
-package com.example.firstapp
+ package com.example.firstapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.firstapp.details.ForecastDetailsActivity
 
-class MainActivity : AppCompatActivity() {
+ class MainActivity : AppCompatActivity() {
+
+
+    // Object of ForecastRepository.
+
+    private val forecastRepository = ForecastRepository()
+    private lateinit var tempDisplaySettingsManager: TempDisplaySettingsManager
+
+    /*Now we have forecastRepository so we can reference it down
+    below in our code
+    */
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        setTitle("Weather")
+
+        tempDisplaySettingsManager = TempDisplaySettingsManager(this)
 
         val zipcode_edittext: EditText = findViewById(R.id.zipcode_editText)
         val submitbutton: Button = findViewById(R.id.submit_button)
 
         submitbutton.setOnClickListener {
 
-            val msg: String =zipcode_edittext.text.toString()
+            val zipcode: String =zipcode_edittext.text.toString()
 
-            if (msg.length != 5)
+            if (zipcode.length != 5)
             {
 
                 Toast.makeText(this,R.string.zipcode_error,Toast.LENGTH_SHORT).show()
@@ -27,12 +52,131 @@ class MainActivity : AppCompatActivity() {
 
             else
             {
-                Toast.makeText(this,msg,Toast.LENGTH_SHORT).show()
+                forecastRepository.loadForecast(zipcode)
             }
 
 
             }
+
+        val forecastListRecyclerView : RecyclerView = findViewById(R.id.forecastListRecyclerView)
+        /*when we will pass our data to recycler view it will display a vertiaclly
+        scrolable list*/
+
+        forecastListRecyclerView.layoutManager = LinearLayoutManager(this)
+
+        val dailyForecastAdapter = DailyForecastAdapter(tempDisplaySettingsManager){
+            /* getString is a special method in activity that lets you
+            access the string resources directly and to get that we need
+            to pass in a string identifier in this case its forecast_clicked_format
+            Now we can pass in any number of aurguments and those aurguments will be
+            used to fill in the string templaate parameters
+            */
+            /*
+            val msg = getString(R.string.forecast_clicked_format,it.temp,it.description)
+            Toast.makeText(this,msg,Toast.LENGTH_SHORT).show()
+             */
+
+
+
+
+            showForecastDetails(it)
+
+        }
+        forecastListRecyclerView.adapter = dailyForecastAdapter
+
+
+
+
+
+
+        /* Observer is an special class in androidx.lifecycle. We made an Observer
+           and items available in its lambda are forecast items. What we need to do
+           with these forecast items is to update our recycler view. For right now
+           this observer will do nothing anytime it will be updated however, now
+           that we have an observer, we can finish the observe implementation below
+        * */
+        val weeklyForcastObserver = Observer<List<DailyForecast>>{ forecastItems->
+
+            //update our list adapter
+            /*
+            submitList will let us send a new list of items which will then update
+            whats on screen
+             */
+            dailyForecastAdapter.submitList(forecastItems)
+
+        }
+
+
+        /*Observer is going to let us know about updates. Each observe methods requires
+          a lifecyle owner. Lifecycle is something that currently knows about there
+          lifecycle. Activity is lifecycle owner. "this" represents enclousing outer
+          class and in this case its MainActivity. Now we passed an observer, so this
+          observer will be updated anytime our live data changes in the repository. And
+          because we have passed that lifecyle observer, all the these changes will be
+          bound to the lifecycle of activity, so if any loading is taking too long, it won't
+          return once the activity is destroyed
+        * */
+
+        forecastRepository.weeklyForecast.observe(this,weeklyForcastObserver)
 
 
     }
+
+
+     // Menu
+
+
+
+     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+         val inflater: MenuInflater = menuInflater
+         inflater.inflate(R.menu.settings_menu, menu)
+         return true
+     }
+
+     /*The OptionsMenu , OptionsItem refer to the options in our menu
+
+      */
+
+
+     /* In this method we are going to pass in a menu item which is going
+     to be whatever item was selected. So we basically check the id that was
+     clicked and respond accordingly
+    */
+
+     override fun onOptionsItemSelected(item: MenuItem): Boolean {
+         return when(item.itemId)
+         {
+             R.id.tempDisplaySetting->
+             {
+                 showTempDisplaySettingDialogue(this,tempDisplaySettingsManager)
+                 true
+             }
+
+             else -> super.onOptionsItemSelected(item)
+         }
+
+     }
+
+
+
+
+     // Menu
+
+
+
+     private fun showForecastDetails(forecast: DailyForecast)
+     { /*Its going to be an explicit intent cuz we are going
+              to tell us what activity to launch. startActivity is
+              method in activity that lets you go from one activity
+              to other
+            */
+         val forecastDetailsIntent = Intent(this,ForecastDetailsActivity::class.java)
+         forecastDetailsIntent.putExtra("key_temp",forecast.temp)
+         forecastDetailsIntent.putExtra("key_description",forecast.description)
+         startActivity(forecastDetailsIntent)
+
+
+     }
+
+
 }
